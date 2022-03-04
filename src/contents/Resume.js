d5bad21e70d0d0fb5a32b9fc760e50a110374b24
teeteathao <87237312+teeteathao@users.noreply.github.com>
2022-03-04 14:20:12 -0800

    add resume and clickable link to save

--- src/contents/Resume.js
+++ src/contents/Resume.js
@@ -8,7 +8,8 @@ class Resume extends Component {
         return (
             <div className="condiv skills">
             {/* <h1 className="subtopic"></h1> */}
-            <img src={ResumeThumbnail} alt="ResumeThumbnail" className="ResumeThumbnail"></img>
+             <a href="https://drive.google.com/file/d/1HDhWHqSX3qyl_Di7Oq4YsbgteLHOHMCq/view?usp=sharings">
+                 <img src={ResumeThumbnail} alt="ResumeThumbnail" className="ResumeThumbnail"></img></a>
             </div>
             )
         }

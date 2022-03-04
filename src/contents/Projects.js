1659692a0ecce2d8eea31f4970b1f363ea54a69c
teeteathao <87237312+teeteathao@users.noreply.github.com>
2022-03-04 15:12:19 -0800

    added projects

--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import Widecard from '../components/Widecard';
+import TripPlanner from '../img/TripPlannerProj.png';
+import GeekSeek from '../img/GeekSeekProj.png';
+import PasswordGen from '../img/PasswordGenProj.png';
+import CalendarProj from '../img/CalendarProj.png';
+import TextEditor from '../img/TextEditor.png';
+
 
 class Projects extends Component {
     render() {
         return (
             <div className="condiv">
-            <h1 className="subtopic">My Education</h1>
-            <Widecard title="M.Tech Software Engineering" where="VIT University" from="July 2017" to="Present"/>
-            <Widecard title="SSLC | HSC" where="Islamiah Higher Secondary School" from="2011" to="2017"/>
+            <a href="https://amdiebolt.github.io/trip-planner/">
+                 <img src={TripPlanner} alt="TripPlanner" className="projectPic"></img></a>
+                 <br></br>
+             <a href="https://infinite-ravine-51041.herokuapp.com/">
+                 <img src={GeekSeek} alt="GeekSeek" className="projectPic"></img></a>
+                 <br></br>
+            <a href="https://teeteathao.github.io/password--generator/">
+                 <img src={PasswordGen} alt="PasswordGen" className="projectPic"></img></a>
+                 <br></br>
+            <a href="https://teeteathao.github.io/API-Scheduler/">
+                 <img src={CalendarProj} alt="CalendarProj" className="projectPic"></img></a>
+                 <br></br>
+            <a href="https://https://github.com/teeteathao/text-editor/">
+                 <img src={TextEditor} alt="TextEditor" className="projectPic"></img></a>
             </div>
             )
         }
